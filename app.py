c30218bd05e221a810ef48591acbf3a969448f81
Vinicius Marcon <134452237+ViniciusMarcon1@users.noreply.github.com>
2025-11-13 22:41:14 +0000

    passos 3 e 4

--- app.py
+++ app.py
@@ -5,12 +5,7 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route("/info")
-def info():
-    return "Nomes: Gustavo Muniz e Vinicius Marcon"
-
-@app.route("/metrica")
-def metrica(): 
+def coletar_metricas():
     pid = os.getpid()
     processo = psutil.Process(pid)
     memoria = processo.memory_info().rss / (1024 * 1024)
@@ -18,16 +13,33 @@ def metrica():
     so_nome = platform.system()
     so_versao = platform.release()
 
-    dados = {
+    return {
         "PID": pid, 
         "Memória Usada": round(memoria, 2), 
-        "% CPU": round(cpu, 2), 
+        "CPU": round(cpu, 2), 
         "Sistema Operacional": f"{so_nome} ({so_versao})"
     }
 
+@app.route("/info")
+def info():
+    return "Nomes: Gustavo Muniz e Vinicius Marcon"
+
+@app.route("/metrica")
+def metrica(): 
+    dados = coletar_metricas()
     return jsonify(dados)
 
+@app.route("/")
+def index():
+    dados = coletar_metricas()
 
+    return (
+        f"Nomes: Gustavo Muniz e Vinicius Marcon<br>"
+        f"PID: {dados['PID']}<br>"
+        f"Memória Usada: {dados['Memória Usada']} MB<br>"
+        f"CPU: {dados['CPU']}%<br>"
+        f"Sistema Operacional: {dados['Sistema Operacional']}"
+    )
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
